a29354b4767bf437ad858e35a01c5ac82e7f6a74
abhishek rathee <abhishekuu10@gmail.com>
2022-04-18 11:17:43 +0530

    modified server for db sync

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const exp = require("express");
 const mongoose = require("mongoose");
-const env = require('dotenv')
-env.config();
+const { application_name } = require("pg/lib/defaults");
+require('dotenv').config()
+const db = require("./data/models");
+ 
+// env.config();
 // const fs = require('fs');
 
 const app = exp();
@@ -9,9 +12,15 @@ const app = exp();
 app.use(exp.json());
 app.use(exp.urlencoded({ extended: true }));
 
+// app.delete('/api/employee/', (req, res, next) => {
+//   console.log(req);
+//   next();
+// })
+
 app.use("/api", require("./routes/api").route);
 // app.use('/employee/:id', require('./delete').deleteEmployee);
 
+db.sequelize.sync();
 app.listen(3050, () => {
-  console.log("server is running at: 3050");
+  console.log(`server is running at: ${process.env.PORT}`);
 });
